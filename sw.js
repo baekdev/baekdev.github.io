0de0ff77c515452b080fd505ad20d0c73e0d729e
BAEKDEV <diveintotechnology@gmail.com>
2020-11-17 00:52:46 +0900

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a5fe66d6561d9c5ef642a7da16b5c8e3"
+    "revision": "ed25a35304827a88001f8d130b90caf9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
