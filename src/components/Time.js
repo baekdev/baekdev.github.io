4f6ea08d2efb14b21da3582891d97a49d5da6247
BAEKDEV <diveintotechnology@gmail.com>
2020-12-02 22:21:46 +0900

    포스팅 날짜 포맷팅 수정

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,30 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
+const moment = require("moment-timezone")
 
 const Time = props => {
-  const TimeContainer = styled.time`
-    color: var(--color-textSecondary);
-  `
-  const { date } = props
+	const TimeContainer = styled.time`
+		color: var(--color-textSecondary);
+	`
+	const { date } = props
+	const formattingDate = moment(date).tz("Asia/Seoul").format("YYYY-MM-DD")
 
-  const dateObject = new Date(date)
-
-  const yyyymmdd = dateObject
-    .toLocaleDateString('ko-KR', {
-      year: 'numeric',
-      month: '2-digit',
-      day: 'numeric',
-    })
-    .split('/')
-    .reverse()
-    .join('-')
-
-  const friendlyDate = dateObject.toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })
-
-  return <TimeContainer datetime={yyyymmdd}>{friendlyDate}</TimeContainer>
+	return <TimeContainer>{`${formattingDate} KST`}</TimeContainer>
 }
 export default Time
